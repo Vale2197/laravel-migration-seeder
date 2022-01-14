642618244ef34b34e3aad663e391860f5a26154b
Vale2197 <valentinatrebbi97@gmail.com>
2022-01-14 18:04:53 +0100

    aggiunta cartella layout, esteso layout di app.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,18 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout.app')
 
-        <title>Laravel</title>
+@section('title', 'welcome')
+    
+@section('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <h1>
+        i nostri pacchetti viaggio:
+    </h1>
 
-        <!-- Styles -->
-    </head>
-    <body>
-        <h1>
-            i nostri pacchetti viaggio:
-        </h1>
-
-        @foreach ($packages as $package)
-            <p>
-                {{$package->id}} - <strong>{{$package->title}}</strong>: <br>
-                <span>{{$package->description}}</span> <span> <strong>price:</strong> {{$package->price}} &#8364; </span>
-            </p>
-        @endforeach
-    </body>
-</html>
+    @foreach ($packages as $package)
+        <p>
+            {{$package->id}} - <strong>{{$package->title}}</strong>: <br>
+            <span>{{$package->description}}</span> <span> <strong>price:</strong> {{$package->price}} &#8364; </span>
+        </p>
+    @endforeach
+    
+@endsection
