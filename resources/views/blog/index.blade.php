e883987cb8490e0c264628b5e78442395e611f1a
Vale2197 <valentinatrebbi97@gmail.com>
2022-01-18 15:31:44 +0100

    creata view riferita al singolo blog (da completare)

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -6,25 +6,30 @@
 @section('content')
 
 <div class="main_blogs">
+    <h2 class="p-5">
+        NEWS:
+    </h2>
 
      <div class="container py-5">
-        <h1>
-            NEWS:
-        </h1>
 
         <div class="row">
             @foreach ($blogs as $blog)
-            <div class="card col-12 my-3">
-                <div class="card-body">
-                    <h5 class="card-title">{{$blog->title}}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">{{$blog->description}}</h6>
-                    <p class="card-text">{{$blog->text}}</p>
-                </div>
-              </div>
+
+            <a href="{{route('blog', ['id' => $blog->id])}}">
+
+                <div class="card col-12 my-3">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$blog->title}}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">{{$blog->description}}</h6>
+                        <p class="card-text">{{$blog->text}}</p>
+                    </div>
+                  </div>
+
+            </a>
             @endforeach
         </div>
 
-        <div class="my_pagination">
+        <div class="my_pagination ps-5">
             {{$blogs->links()}}
         </div>
     </div>
---
+++ resources/views/blog/show.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.app')
+
+@section('content')
+
+    <div class="main_blog">
+
+        
+    </div>
+
+@endsection
